214b866d799d7e73513c2f337abe0b9c3ea62ac0
shinbumjun <https://github.com/shinbumjun>
2024-09-29 19:07:06 +0900

    테스트

--- src/main/java/com/game/community/server/ServerApplication.java
+++ src/main/java/com/game/community/server/ServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("테스트");
 		SpringApplication.run(ServerApplication.class, args);
 	}
 

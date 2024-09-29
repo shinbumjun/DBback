c623bb138897915df9b0a5b1fff343ad0b1b5975
shinbumjun <https://github.com/shinbumjun>
2024-09-29 17:07:11 +0900

    처음 설정 관련 수정

--- src/main/java/com/game/community/server/ServerApplication.java
+++ src/main/java/com/game/community/server/ServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("11111111111111111111111111111");
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
